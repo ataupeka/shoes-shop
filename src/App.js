dfbac5851a6c281951b5e953868680b86a84ace6
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-04-23 12:51:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from "react";
+
+import {
+
+  BrowserRouter as Router,
+
+  Routes,
+
+  Route,
+
+  Link
+
+} from "react-router-dom";
+
+
+import PageOne from "./components/PageOne";
+import PageItemsForMen from "./components/PageItemsForMen";
+import PageItemsforWomen from "./components/PageItemsForWomen";
+import Footer from "./components/Footer";
+
+
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+
+function App() {
+
+  return(
+    <div>  
+  <Router> 
+ <nav>
+ <Link className="navLink" to="/home_page"> Home Page </Link>
+  <Link className="navLink" to="/items_for_women"> Femme </Link>
+  <Link className="navLink" to="/items_for_men"> Homme </Link>
+  
+</nav>
+
+<Routes>
+<Route path="/home_page" element={<PageOne/>}/>
+<Route path="/items_for_women" element={<PageItemsforWomen/>}/>
+<Route path="/items_for_men" element={<PageItemsForMen/>}/>
+
+
+</Routes>
+
+ </Router>
+ <Footer/>
+
+ </div>
+
+
+
+  ); 
+ 
+
+
+}
 export default App;
