8474ee1902549a387dc8f42bb491226529c650b5
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-04-23 12:59:47 -0400

    Add files via upload

--- src/data/dataManItems.js
+++ src/data/dataManItems.js
@@ -1,1 +1,43 @@
+import menShoesItem1 from '../images/men-shoes-image1.jpg';
+import menShoesItem2 from '../images/men-shoes-image2.jpg';
+import menShoesItem3 from '../images/men-shoes-image3.jpg';
+
+
+
+export const dataManItems= [
+    {
+    id:1,
+    name: "Oxford Shoes",
+    searchTerm:"shoes",
+    price: 39.99,
+    image: menShoesItem1,
+    articleNumber: 123456,
+    brand: "shagavita",
+    },
+
+    {
+        id:2,
+        name: "Running Shoes",
+        searchTerm:"sport",
+        price: 54.99,
+        image: menShoesItem2,
+        articleNumber: 123457,
+        brand: "sport-life",
+        },
+    {
+        id:3,
+        name: "Boots",
+        searchTerm:"boots",
+        price: 69.99,
+        image: menShoesItem3,
+        articleNumber: 123459,
+        brand: "D&K",
+        },                     
+
+]
+                           
+
+export default dataManItems;
+    
+
 
---
+++ src/data/dataWomenItems.js
@@ -0,0 +1,39 @@
+import womenShoesItem1 from '../images/women-shoes-image1.jpg';
+import womenShoesItem2 from '../images/women-shoes-image2.jpg';
+import womenShoesItem3 from '../images/women-shoes-image3.jpg';
+
+
+
+export const dataWomenItems = [
+    {
+    id:1,
+    name: "Running shoes",
+    searchTerm:"sport",
+    price: 59.99,
+    image: womenShoesItem1,
+    articleNumber: 234567,
+   brand: "sport-life",
+    },
+
+    {
+        id:2,
+        name: "Running shoes",
+        searchTerm:"sport",
+        price: 59.99,
+        image: womenShoesItem2,
+        articleNumber: 234894,
+        brand: "shagavita",
+        },
+              
+        {
+            id:3,
+            name: "Running shoes",
+            searchTerm:"sport",
+            price: 59.99,
+            image: womenShoesItem3,
+            articleNumber: 238970,
+            brand: "D&K",
+            }   
+
+
+]
