84278d9c0642a7a3106a12a10bf3701067d34a66
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-04-23 12:56:01 -0400

    to add Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,1 +1,14 @@
+function Buttons({filteredItem}){
+    return(
+<div className="container">
+<button className='btn' onClick={() => filteredItem ("boots")}> Boots </button>
+<button className="btn" onClick={() => filteredItem ("shoes")}> Shoes </button>
+<button  className="btn"onClick={() => filteredItem ("sport")}> Sport shoes </button>
 
+
+
+</div>
+)
+}
+
+export default Buttons;
