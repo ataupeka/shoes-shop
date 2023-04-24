9a9a78c25b9a7046eb0df66367f7ad7f440a4f30
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-04-24 17:38:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,45 @@
 import React from "react";
 
-import {
-
-  BrowserRouter as Router,
-
-  Routes,
-
-  Route,
-
-  Link
-
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import PageOne from "./components/PageOne";
 import PageItemsForMen from "./components/PageItemsForMen";
 import PageItemsforWomen from "./components/PageItemsForWomen";
 import Footer from "./components/Footer";
 
-
-
-
-import './App.css';
-
-
-
-
+import "./App.css";
 
 function App() {
+  return (
+    <div>
+      <Router>
+        <nav>
+          <Link className="navLink" to="/home_page">
+            {" "}
+            Home Page{" "}
+          </Link>
+          <Link className="navLink" to="/items_for_women">
+            {" "}
+            Femme{" "}
+          </Link>
+          <Link className="navLink" to="/items_for_men">
+            {" "}
+            Homme{" "}
+          </Link>
+          <Link className="navLink" to="/items_for_men">
+            {" "}
+            Sales{" "}
+          </Link>
+        </nav>
 
-  return(
-    <div>  
-  <Router> 
- <nav>
- <Link className="navLink" to="/home_page"> Home Page </Link>
-  <Link className="navLink" to="/items_for_women"> Femme </Link>
-  <Link className="navLink" to="/items_for_men"> Homme </Link>
-  
-</nav>
-
-<Routes>
-<Route path="/home_page" element={<PageOne/>}/>
-<Route path="/items_for_women" element={<PageItemsforWomen/>}/>
-<Route path="/items_for_men" element={<PageItemsForMen/>}/>
-
-
-</Routes>
-
- </Router>
- <Footer/>
-
- </div>
-
-
-
-  ); 
- 
-
-
+        <Routes>
+          <Route path="/home_page" element={<PageOne />} />
+          <Route path="/items_for_women" element={<PageItemsforWomen />} />
+          <Route path="/items_for_men" element={<PageItemsForMen />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 export default App;
