509e0be2863be8ea0631c82b9b8b535094fdfec1
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-04-24 17:36:32 -0400

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,23 +1,42 @@
 import React from "react";
+import image1 from "../images/main-image1.jpg";
+
+import image2 from "../images/main-image2.jpg";
 
 const Main = () => (
-    <div className="main">
-    <main class="">
-        <section class="top">
-          <div class="main-container">
-           <div>  <h1 class="title main-p"> Spring Sale</h1> </div>
-           
-           <div> <p class="main-p">Buy 1 and get -30% off</p> </div>
-           <div> <p class="main-p">Buy 2 and get -40% off</p> </div>
-           <div>  <p class="main-p">Buy 3 and get -50% off</p> </div>
-           <div>  <p class="main-p">on all regular prices</p> </div>
-           <div>   <button> Shop now </button> </div>
+  <div>
+    <main class="main">
+      <div class="main-container">
+        <div className="center">
+          {" "}
+          <h1 class="main-title "> Spring Sale</h1>{" "}
+        </div>
 
-          </div>
-        </section>
-        
-</main>
-    
+        <div className="center">
+          {" "}
+          <p class="main-p">Buy 1 and get -30% off</p>{" "}
+        </div>
+        <div className="center">
+          {" "}
+          <p class="main-p">Buy 2 and get -40% off</p>{" "}
+        </div>
+        <div className="center">
+          {" "}
+          <p class="main-p">Buy 3 and get -50% off</p>{" "}
+        </div>
+        <div className="center">
+          {" "}
+          <p class="main-p">on all regular prices</p>{" "}
+        </div>
+        <div className="center">
+          {" "}
+          <a href="">
+            {" "}
+            <button className="main-button"> SHOP NOW </button>{" "}
+          </a>{" "}
+        </div>
+      </div>
+    </main>
   </div>
 );
 
